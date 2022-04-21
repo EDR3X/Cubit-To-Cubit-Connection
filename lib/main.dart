b0b23749b27c421eea8b890382ee110deeda76ab
EDR3X <dhunganaannanta@gmail.com>
2022-04-21 20:15:00 +0545

    MultiBlocProvider initiated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:cubit_to_cubit_communication/cubits/color/color_cubit.dart';
+import 'package:cubit_to_cubit_communication/cubits/counter/counter_cubit.dart';
 import 'package:cubit_to_cubit_communication/homepage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,11 +11,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Cubit to Cubit Communication",
-      theme: ThemeData(primarySwatch: Colors.green),
-      home: const HomePage(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<ColorCubit>(
+          create: (context) => ColorCubit(),
+        ),
+        BlocProvider<CounterCubit>(
+          create: ((context) => CounterCubit(
+                colorCubit: context.read<ColorCubit>(),
+              )),
+        )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Cubit to Cubit Communication",
+        theme: ThemeData(primarySwatch: Colors.green),
+        home: HomePage(),
+      ),
     );
   }
 }
